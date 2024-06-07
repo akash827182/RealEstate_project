2559b14b3c853b1021cc7a83cfe14b0017c641aa
Akash Kumar <akashkumar827182@gmail.com>
2024-06-07 19:04:32 +0530

    residencies added

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
 import "./App.css";
 import Companies from "./components/companies/companies";
-// import Residencies from "./components/Residencies/Residencies";
+import Residencies from "./components/Residencies/Residencies";
 function App() {
   return (
     <div className="App">
@@ -12,7 +12,7 @@ function App() {
         <Hero />
       </div>
       <Companies />
-      {/* <Residencies /> */}
+      <Residencies />
     </div>
   );
 }
